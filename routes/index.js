5c06bab7815072d491d1950c71ce26f518c89373
root <ninthflooor@gmail.com>
2023-04-04 22:59:43 +0900

    Create POST

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-const blogs = require("../models/blogs");
+const Blog = require("../models/blogs");
 
 module.exports = (app) => {
      // GET ALL blogs
      app.get('/api/blogs', function(req,res){
-        blogs.find((err, blogs) => {
+        Blog.find((err, blogs) => {
             if(err) return res.status(500).send({error : "database failure"});
             res.json(blogs)
         })
@@ -17,9 +17,18 @@ module.exports = (app) => {
         res.end();
     });
     // CREATE BOOK
-    app.post('/api/blogs', function(req, res){
-        res.end();
+    app.post('/api/blogs/add', function(req, res){
+        const postblog = new Blog({
+            title: req.body.title,
+            body: req.body.body,
+            author: req.body.author
+        });
 
+        postblog.save((error, postblog) => {
+            if(error) return console.log(error)
+            console.dir(postblog);
+        });
+        
     });
     // UPDATE THE BOOK
     app.put('/api/blogs/:blog_id', function(req, res){
